24fa0c712c80b0431a4042dd782de04a0a374c9c
StarBr3ad <150461968+StarBr3ad@users.noreply.github.com>
2025-11-12 20:22:32 +0800

    pagination update

---
+++ migrations/Version20251014111620.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20251014111620 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE color (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, price DOUBLE PRECISION NOT NULL, image_path VARCHAR(255) DEFAULT NULL, availability TINYINT(1) NOT NULL, color_hex VARCHAR(7) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE finish (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, details LONGTEXT NOT NULL, price DOUBLE PRECISION NOT NULL, properties LONGTEXT NOT NULL, availability TINYINT(1) NOT NULL, image_path VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE material (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, details LONGTEXT NOT NULL, price DOUBLE PRECISION NOT NULL, properties LONGTEXT NOT NULL, availability TINYINT(1) NOT NULL, image_path VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, full_name VARCHAR(255) NOT NULL, address LONGTEXT NOT NULL, email VARCHAR(255) NOT NULL, created_at DATETIME NOT NULL, is_admin TINYINT(1) NOT NULL, UNIQUE INDEX UNIQ_8D93D649F85E0677 (username), UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user_order (id INT AUTO_INCREMENT NOT NULL, material_id INT NOT NULL, finish_id INT NOT NULL, color_id INT NOT NULL, user_id INT NOT NULL, filename VARCHAR(255) NOT NULL, file_path VARCHAR(255) NOT NULL, order_state VARCHAR(255) NOT NULL, delievery_date DATE NOT NULL, delivery_arrival DATE NOT NULL, price_total NUMERIC(10, 2) NOT NULL, created_at DATETIME NOT NULL, INDEX IDX_17EB68C0E308AC6F (material_id), INDEX IDX_17EB68C02B4667EB (finish_id), INDEX IDX_17EB68C07ADA1FB5 (color_id), INDEX IDX_17EB68C0A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', available_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', delivered_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE user_order ADD CONSTRAINT FK_17EB68C0E308AC6F FOREIGN KEY (material_id) REFERENCES material (id)');
+        $this->addSql('ALTER TABLE user_order ADD CONSTRAINT FK_17EB68C02B4667EB FOREIGN KEY (finish_id) REFERENCES finish (id)');
+        $this->addSql('ALTER TABLE user_order ADD CONSTRAINT FK_17EB68C07ADA1FB5 FOREIGN KEY (color_id) REFERENCES color (id)');
+        $this->addSql('ALTER TABLE user_order ADD CONSTRAINT FK_17EB68C0A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE user_order DROP FOREIGN KEY FK_17EB68C0E308AC6F');
+        $this->addSql('ALTER TABLE user_order DROP FOREIGN KEY FK_17EB68C02B4667EB');
+        $this->addSql('ALTER TABLE user_order DROP FOREIGN KEY FK_17EB68C07ADA1FB5');
+        $this->addSql('ALTER TABLE user_order DROP FOREIGN KEY FK_17EB68C0A76ED395');
+        $this->addSql('DROP TABLE color');
+        $this->addSql('DROP TABLE finish');
+        $this->addSql('DROP TABLE material');
+        $this->addSql('DROP TABLE user');
+        $this->addSql('DROP TABLE user_order');
+        $this->addSql('DROP TABLE messenger_messages');
+    }
+}
